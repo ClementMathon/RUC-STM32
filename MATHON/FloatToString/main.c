bc3e1eb78385636dd149d8af79b8a5c4d0f6b289
ClementMathon <mathon.clement.lgm@gmail.com>
2017-04-05 15:57:36 +0200

    Add files via upload

--- MATHON/FloatToString/main.c
+++ MATHON/FloatToString/main.c
@@ -4,10 +4,13 @@
 #include <math.h>
 #include <string.h>
 
-void setPoids(char* trame, float poids)
+void setPoids(unsigned char* trame, float* ptPoids)
 {
-  char* pds[9];
-  *pds = ( char* ) & poids;
+  int i = 7;
+  float poids = *ptPoids;
+
+  //char* pds[9];
+  unsigned char *pds = ( unsigned char* ) & poids;
   trame[7] = pds[0];
   trame[8] = pds[1];
   trame[9] = pds[2];
@@ -16,12 +19,21 @@ void setPoids(char* trame, float poids)
   trame[12] = pds[5];
   trame[13] = pds[6];
   trame[14] = pds[7];
+
+  printf("poids       = ");
+  while(i<11){
+    printf("%2.2X ",trame[i]);
+    i++;
+  }
+  printf("\n\n");
 }
 
-void setTemperature(char* trame, float temper)
+void setTemperature(unsigned char* trame, float * ptTemper)
 {
-  char* temp[9];
-  *temp = ( char* ) & temper;
+  float temperature = *ptTemper;
+  int i = 16;
+  //char* temp[9];
+  unsigned char *temp = ( unsigned char* ) & temperature;
   trame[16] = temp[0];
   trame[17] = temp[1];
   trame[18] = temp[2];
@@ -30,12 +42,20 @@ void setTemperature(char* trame, float temper)
   trame[21] = temp[5];
   trame[22] = temp[6];
   trame[23] = temp[7];
+
+  printf("temperature = ");
+  while(i<20){
+    printf("%2.2X ",trame[i]);
+    i++;
+  }printf("\n\n");
 }
 
-void setHygrometrie(char* trame, float hygro)
+void setHygrometrie(unsigned char* trame, float * ptHygro)
 {
-  char* humy[9];
-  *humy = ( char* ) & hygro;
+  int i = 25;
+  float hygrometrie = *ptHygro;
+  //char* humy[9];
+  unsigned char *humy = ( unsigned char* ) & hygrometrie;
   trame[25] = humy[0];
   trame[26] = humy[1];
   trame[27] = humy[2];
@@ -44,12 +64,18 @@ void setHygrometrie(char* trame, float hygro)
   trame[30] = humy[5];
   trame[31] = humy[6];
   trame[32] = humy[7];
+
+  printf("hygrometrie = ");
+  while(i<29){
+    printf("%2.2X ",trame[i]);
+    i++;
+  }printf("\n\n");
 }
 
-void sendTrame(float poids, float temp, float hygro)
+void sendTrame(float* ptTest, float* ptTest2, float* ptTest3)
 {
   int i = 0;
-  char * trame[36];
+  unsigned char trame[36];
 
   trame[0] = 'A';
   trame[1] = 'T';
@@ -63,12 +89,19 @@ void sendTrame(float poids, float temp, float hygro)
   trame[33] = '\n';
   trame[34] = '\r';
 
-  setPoids(&trame, poids);
-  setTemperature(&trame, temp);
-  setHygrometrie(&trame, hygro);
-
+  setPoids(trame, ptTest);
+  setTemperature(trame, ptTest2);
+  setHygrometrie(trame, ptTest3);
+  i = 0;
   while(i<34){
-    printf("%s",trame[i]);
+        if((i<=6)||(i==15)||(i==24)||(i>=33))
+        {
+            printf("%c",trame[i]);
+        }if(((i>=7)&&(i<11))||((i>=16)&&(i<20))||((i>=25)&&(i<29)))
+        {
+            printf("%2.2X",trame[i]);
+        }
+
     i++;
   }
 }
@@ -80,12 +113,13 @@ int main()
     float test2 = 28.9;
     float test3 = 12.1;
 
-    printf("float to string\n\n");
+    float * ptTest = &test;
+    float * ptTest2 = &test2;
+    float * ptTest3 = &test3;
 
-    sendTrame(test, test2, test3);
-
-    printf("%f\n\n",test);
-    printf("%#08.8X \n\n",( char* ) & test);
+    printf("float to string + AT command :\n\n");
+    sendTrame(ptTest, ptTest2, ptTest3);
+    //printf("%f\n\n",test);
 
     return 0;
 }
